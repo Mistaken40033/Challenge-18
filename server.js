aaf1bb8ee99fb2dec277511799681e548026328b
Erica Balderas <ericab40033@hotmail.com>
2024-07-01 22:13:46 -0600

    making files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/api/userRoutes');
+const thoughtRoutes = require('./routes/api/thoughtRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/challenge-18', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
---
+++ utils/dateFormat.js
@@ -0,0 +1,6 @@
+const dateFormat = (timestamp) => {
+    return new Date(timestamp).toLocaleString();
+  };
+  
+  module.exports = dateFormat;
+  
